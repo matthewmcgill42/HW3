b6645e600090d26c9ba88d8a03835bb093a130cd
matthewmcgill42 <matt.mcgillivray@kellogg.northwestern.edu>
2026-05-22 03:43:38 +0000

    changes

---
+++ app/controllers/place_controller.rb
@@ -0,0 +1,23 @@
+class PlacesController < ApplicationController
+
+  def index
+    @places = Place.all
+  end
+
+  def show
+    @place = Place.find(params[:id])
+    @entries = @place.entries
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new
+    @place.name = params[:name]
+    @place.save
+    redirect_to("/places/#{@place.id}")
+  end
+
+end
